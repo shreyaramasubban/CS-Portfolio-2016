d14aa5f4ed20b49f79cb49f55dad6b792a6d98e6
Shreya Ramasubban <sramasubban.ra@mysummitps.org>
2016-12-04 10:40:00 +0000

    created calculator howto page

--- calculator.js
+++ calculator.js
@@ -1,15 +1,20 @@
 // JavaScript File
+//These are the variables that I used to id in my html.
 var num1 = document.getElementById("num1");
 var num2 = document.getElementById("num2");
 var operation = document.getElementById("operation");
 var calculateButton = document.getElementById("calculateButton");
 var answer = document.getElementById("answer");
 
+//This is the function that sets the input from the user to its value.
+//This function calculates the answer for each operation.
 calculateButton.addEventListener("click", function() {
     var inputnum1 = Number (num1.value);
     var inputnum2 = Number (num2.value);
     var inputoperation = operation.value;
 
+//These statements calculate the answer for each operation. 
+    
     if (inputoperation == "+") {
 
         var ans = inputnum1 + inputnum2;
--- todolist.js
+++ todolist.js
@@ -1,10 +1,12 @@
 // JavaScript File
 
+// These are the variables that are the id in the html.
 var addButton = document.getElementById("addButton");
 var list = document.getElementById("list");
 var task = document.getElementById("task");
 var color = document.getElementById("color");
 
+//This is the function that lets the user add items to the list in specific colors.
 addButton.addEventListener('click', function (){
     var inputColor = color.value;
     var inputTask = task.value;
@@ -12,17 +14,20 @@ addButton.addEventListener('click', function (){
     
 });
 
+//This is the function that removes an item from the list when you double click it.
 list.addEventListener('dblclick', function(evt){
     var removeTask = evt.target;
     removeTask.parentNode.removeChild(removeTask);
 
 });
 
+//This is the function that strikes through an item on the list when you single click it. 
 list.addEventListener('click', function(evt){
     var strikeTask = document.getElementById("list");
     var strikeTask = evt.target;
     strikeTask.style.setProperty("text-decoration", "line-through");
 });
+
 
 
 
