426b9bf6598adbbbb914109803e4875fa34ccdba
Shreya Ramasubban <sramasubban.ra@mysummitps.org>
2016-12-05 03:42:52 +0000

    created calculator howto page

--- calculator.js
+++ calculator.js
@@ -1,12 +1,12 @@
 // JavaScript File
-//These are the variables that I used to id in my html.
+//These variables each correspond to the id given in the html.
 var num1 = document.getElementById("num1");
 var num2 = document.getElementById("num2");
 var operation = document.getElementById("operation");
 var calculateButton = document.getElementById("calculateButton");
 var answer = document.getElementById("answer");
 
-//This is the function that sets the input from the user to its value.
+//This is the function that sets the user input to its value. 
 //This function calculates the answer for each operation.
 calculateButton.addEventListener("click", function() {
     var inputnum1 = Number (num1.value);
@@ -47,6 +47,7 @@ calculateButton.addEventListener("click", function() {
         alert(ans);
     }
     
+//In this statement, inputnum1 is the base and inputnum2 is the power.    
     if (inputoperation == "power of") {
 
         var ans = Math.pow(inputnum1,inputnum2);
